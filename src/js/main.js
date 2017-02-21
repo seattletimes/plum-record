be885edd2a4cb50130878bd9db1947e52cf72dbd
Thomas Wilburn <news@thomaswilburn.net>
2017-02-21 15:05:29 -0800

    All hail the hypnotoad

--- src/js/main.js
+++ src/js/main.js
@@ -2,4 +2,34 @@
 // require("./lib/ads");
 // var track = require("./lib/tracking");
 
-require("component-responsive-frame/child");
+var $ = require("./lib/qsa");
+
+var scrollContainer = $.one(".scroll-graph");
+var seasons = $(".season");
+var debug = $.one(".debug");
+
+var onScroll = function() {
+  var season = null;
+  var bounds = null;
+  for (var i = 0; i < seasons.length; i++) {
+    var s = seasons[i];
+    var b = s.getBoundingClientRect();
+    if (b.top < 0) {
+      s.classList.add("active");
+    } else {
+      s.classList.remove("active");
+    }
+    if (b.top <= window.innerHeight && b.bottom > 0 && !season) {
+      season = s;
+      bounds = b;
+    }
+  }
+  if (!bounds) return;
+  var progress = bounds.top > 0 ? 0 : 1 - ((bounds.height + bounds.top) / bounds.height);
+  if (progress > 1) progress = 1;
+  var num = season.getAttribute("data-season");
+  debug.innerHTML = `Season ${num}, ${(progress * 100).toFixed(1)}%`;
+}
+
+window.addEventListener("scroll", onScroll);
+onScroll();
