35653fc619d7df2947cd2c71d411f691b0687f38
Thomas Wilburn <news@thomaswilburn.net>
2017-02-22 14:58:40 -0800

    different season coloring

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@
 // var track = require("./lib/tracking");
 
 var $ = require("./lib/qsa");
+var colors = require("./lib/colors");
 
 var scrollContainer = $.one(".scroll-graph");
 var seasons = $(".season");
@@ -18,6 +19,13 @@ var maxPoints = gameData[gameData.length - 1].aggregate;
 
 var canvas = $.one(".graph");
 var context = canvas.getContext("2d");
+
+var palette = {
+  1: colors.palette.stLightRed,
+  2: colors.palette.stLightOrange,
+  3: colors.palette.stLightGreen,
+  4: colors.palette.stLightBlue
+}
 
 var onScroll = function() {
   var season = null;
@@ -39,25 +47,42 @@ var onScroll = function() {
   var progress = bounds.top > 0 ? 0 : 1 - ((bounds.height + bounds.top) / bounds.height);
   if (progress > 1) progress = 1;
   var num = season.getAttribute("data-season");
-  debug.innerHTML = `Season ${num}, ${(progress * 100).toFixed(1)}%`;
 
   var seasonData = bySeason[num];
   var index = Math.floor(seasonData.length * progress);
   var final = seasonData[index];
 
+  debug.innerHTML = `Season ${num}, game ${index + 1} of ${seasonData.length}`;
+
   canvas.width = canvas.offsetWidth;
   canvas.height = canvas.offsetHeight;
-  context.strokeStyle = "white";
+  context.strokeStyle = palette[1];
   context.lineWidth = 4;
   context.beginPath();
   context.moveTo(0, canvas.height);
+  var state = {
+    season: 1,
+    x: 0,
+    y: 0
+  }
 
   for (var i = 0; i < gameData.length; i++) {
     var game = gameData[i];
 
-    var x = (i / gameData.length) * canvas.width;
+    var x = (i + 1) / gameData.length * canvas.width;
     var y = canvas.height - (game.aggregate / maxPoints * canvas.height);
     context.lineTo(x, y);
+
+    if (state.season != game.season) {
+      context.stroke();
+      context.beginPath();
+      context.moveTo(state.x, state.y);
+      context.strokeStyle = palette[game.season];
+      state.season = game.season;
+    }
+
+    state.x = x;
+    state.y = y;
 
     if (game == final) break;
   }
